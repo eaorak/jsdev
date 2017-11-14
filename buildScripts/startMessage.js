c58fcee09ab0c19d82cb5083b94ab4fc620a7eb5
Ender Aydin Orak <eaorak@gmail.com>
2017-11-14 10:36:18 +0000

    js dev

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log(chalk.green("App is starting...")); //eslint-disable-line no-console
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+import chalk from 'chalk';
+
+/* eslint-disable no-console */
+
+const name = 'Ender';
+const year = 2017;
+debugger; // eslint-disable-line no-debugger
+console.log(chalk.red(`Hi ${name}. This is index at ${year}!`));
